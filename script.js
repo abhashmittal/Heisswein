f8cc1e49ba95f9e4e6a471a96e8ceebcd9e0ead0
abhashmittal <160855129+abhashmittal@users.noreply.github.com>
2024-02-27 14:20:04 +0100

    noinoi

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const viewer = new PhotoSphereViewer.Viewer({
   container: document.querySelector('#viewer'),
-  panorama: 'assets/umbau.jpg',
+  panorama: 'assets/umbau4.jpg',
   defaultZoomLvl: '30',
 });
 

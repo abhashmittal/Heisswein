32b9a47f35c3c1234e93bbb9c8008d22c584f22a
abhashmittal <160855129+abhashmittal@users.noreply.github.com>
2024-02-27 12:25:39 +0100

    reuppp

---
+++ script.js
@@ -0,0 +1,14 @@
+const viewer = new PhotoSphereViewer.Viewer({
+  container: document.querySelector('#viewer'),
+  panorama: 'assets/umbau.jpg',
+  defaultZoomLvl: '30',
+});
+
+
+var mybtn = document.querySelector("#button")
+var slide = document.querySelector("#slide")
+var info = document.querySelector("#info")
+
+mybtn.addEventListener("click", function() {
+  slide.classList.toggle("show")
+});

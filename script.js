ae53ba5825ec3d71e2e9e37736ed0b7b0320773e
abhashmittal <160855129+abhashmittal@users.noreply.github.com>
2024-02-27 11:18:08 +0100

    reupp

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const viewer = new PhotoSphereViewer.Viewer({
 
 var mybtn = document.querySelector("#button")
 var slide = document.querySelector("#slide")
+var info = document.querySelector("#info")
 
 mybtn.addEventListener("click", function() {
   slide.classList.toggle("show")

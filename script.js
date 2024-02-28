e53d42bf01b105e0b4912f344e7bb72d9fc2aa17
abhashmittal <160855129+abhashmittal@users.noreply.github.com>
2024-02-28 15:44:42 +0100

    irritation

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const viewer = new PhotoSphereViewer.Viewer({
   container: document.querySelector('#viewer'),
-  panorama: 'assets/umbau4.jpg',
+  panorama: 'assets/umbau5.jpg',
   defaultZoomLvl: '30',
 });
 

65cc9a6cf08321a080b6be99b61119cb59dfaa61
abhashmittal <160855129+abhashmittal@users.noreply.github.com>
2025-01-31 17:36:22 +0100

    Pop-Up Implementation

--- script.js
+++ script.js
@@ -8,7 +8,55 @@ const viewer = new PhotoSphereViewer.Viewer({
   defaultZoomLvl: '20',
 });
 
+var popup = document.querySelector("#pop")
+var closepopup = document.querySelector("#button4")
+var sitenav = document.querySelector("#sitenav")
+var karte = document.querySelector(".popupbg")
 
+
+async function checkImageExists(imagePath) {
+  try {
+    const response = await fetch(imagePath, { method: 'HEAD' });
+    return response.ok; // true if the image exists
+  } catch (error) {
+    let toggleContentBasedOnImage = ('assets/double-trouble_23.png');
+  }
+}
+
+// Function to toggle HTML based on image existence
+async function toggleContentBasedOnImage(imagePath) {
+  const contentDiv = document.getElementById('annonce');
+  const imageExists = await checkImageExists(imagePath);
+
+  if (imageExists) {
+ 
+  } else {
+    popup.classList.toggle('active');
+    sitenav.classList.toggle('active');
+    karte.classList.toggle('active');
+  }
+}
+
+popup.addEventListener("click", function(){
+  if ($(".popupbg:hover").length !=0){
+  } else {
+    popup.classList.toggle('active');
+    sitenav.classList.toggle('active');
+    karte.classList.toggle('active');
+  }
+}
+)
+
+// Call the function with the path to the image
+toggleContentBasedOnImage('popup/scan.jpg');
+
+closepopup.addEventListener("click", function() {
+  popup.classList.toggle('active');
+  sitenav.classList.toggle('active');
+  karte.classList.toggle('active');
+});
+
+//navigation triggers when there is no Pop-Up present
 var mybtn = document.querySelector("#button")
 var slide = document.querySelector("#slide")
 var info = document.querySelector("#info")
@@ -16,8 +64,8 @@ var nav = document.querySelector("#nav")
 var view = document.querySelector("#viewer")
 var overlay = document.querySelector("#overlay")
 
-mybtn.addEventListener("click", function() {
 
+mybtn.addEventListener("click", function() {
   slide.classList.toggle("show");
   mybtn.classList.toggle('active');
   info.classList.toggle('active');
@@ -26,9 +74,7 @@ mybtn.addEventListener("click", function() {
 });
 
 slide.addEventListener("click", function(){
-
   if ($(".konzept:hover").length != 0){
-
   } else {
   slide.classList.toggle("show");
   mybtn.classList.toggle('active');
@@ -36,10 +82,7 @@ slide.addEventListener("click", function(){
   nav.classList.toggle('active');
   overlay.classList.toggle('active')
 }
-
 });
-
-
 
 function orientationChange() {
   if(window.addEventListener) {
@@ -47,4 +90,5 @@ function orientationChange() {
           location.reload();
       });
   }
-}
+};
+
